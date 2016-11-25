3b9a153f3853be45d92cfa53d4365aafd1cffb5e
Daniel Ödman <danielodman@outlook.com>
2016-11-25 11:17:20 +0100

    Small changes to Boundarymap

--- Boundarymap.py
+++ Boundarymap.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 
 class Boundarymaps:
-    
+    def boundaryMap1(self):
+        boundarymap1 = np.zeros((10,10), dtype = 'bool')
+        boundarymap1[0,:] = 1 
+        boundarymap1[9,:] = 1
+        boundarymap1[:,0] = 1
+        boundarymap1[:,9] = 1
+        return boundarymap1
+
+if __name__ == "__main__":
+    bMap = Boundarymaps()
+    map = bMap.boundaryMap1()
+    print(map)
     
